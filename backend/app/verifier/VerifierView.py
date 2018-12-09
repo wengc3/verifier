89421e826eca56796fae809877f909ae0e53ccf2
Christian Wenger <wengc3@bfh.ch>
2018-12-09 12:56:59 +0100

    fix json

--- backend/app/verifier/VerifierView.py
+++ backend/app/verifier/VerifierView.py
@@ -1,5 +1,6 @@
 from chvote.verifier.Observer import Observer
 from app.api.syncService import emitToClient, SyncType
+import json
 
 class VerifierView(Observer):
     """docstring for VerifierView."""
@@ -21,14 +22,16 @@ class VerifierView(Observer):
         test = self.result.test
         emitToClient('testRunning',test.title,SyncType.ROOM,self.report.electionID)
         if test.id.count('.') == 0:
-            emitToClient('newState',"{'id': id,'value': 'running'}",SyncType.ROOM,self.report.electionID)
+            data = json.dumps({'id': id,'value': 'running'})
+            emitToClient('newState',data,SyncType.ROOM,self.report.electionID)
 
     def _newProgress(self):
         id = self.result.test.id
         if id.count('.') == 0:
             prg = self.result.progress
             if prg == 1:
-                emitToClient('newState',{'id': id,'value': 'completed'},SyncType.ROOM,self.report.electionID)
+                data = json.dumps({'id': id,'value': 'completed'})
+                emitToClient('newState',data,SyncType.ROOM,self.report.electionID)
             newprg = int(20*prg) + ((int(id) - 1)*20)
             emitToClient('newProgress',str(newprg),SyncType.ROOM,self.report.electionID)
 
