1ed78e96fc3d5436dc970fc1ac69a8aba8fde988
Christian Wenger <wengc3@bfh.ch>
2019-01-15 09:27:51 +0100

    multi result had wrong lenght

--- frontend/src/mixins/getTotalChildrenMixin.js
+++ frontend/src/mixins/getTotalChildrenMixin.js
@@ -1,7 +1,14 @@
 export default {
   methods: {
     getTotalChildren: function (result) {
-      return result.children.length
+      if (result.children.length > 0) {
+        if (result.children[0].children.length > 0) {
+          return result.children[0].children.length
+        } else {
+          return result.children.length
+        }
+      }
+      return 0
     }
   }
 }
