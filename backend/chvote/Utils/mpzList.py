07e259ca4179713dd40a2fd324709406bbff2ff5
Christian Wenger <wengc3@bfh.ch>
2019-01-16 09:22:44 +0100

    add comment

--- backend/chvote/Utils/mpzList.py
+++ backend/chvote/Utils/mpzList.py
@@ -2,6 +2,6 @@ from gmpy2 import mpz
 
 def mpzList(data_list):
     """
-    transform a list with numbers to a list with mpz values
+    Transform a list with numbers to a list with mpz values
     """
     return [mpz(item) for item in data_list]
